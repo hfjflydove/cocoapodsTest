4d98f1a687ee7235659c73d3b71c2a1d1070a394
hefeijiang <hefeijiang@silversnet.com>
2015-10-29 14:00:52 +0800

    first commit

---
+++ githubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubTest
+//
+//  Created by 何飞江 on 15/10/29.
+//  Copyright © 2015年 何飞江. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ githubTest/lib/SdkCocoapodsTest.h
@@ -0,0 +1,15 @@
+//
+//  SdkCocoapodsTest.h
+//  modalTest
+//
+//  Created by 何飞江 on 15/10/29.
+//  Copyright © 2015年 何飞江. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SdkCocoapodsTest : NSObject
+
+- (void)testLog;
+
+@end
